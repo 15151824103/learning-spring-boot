ec09bf1c30420e4f8049534249838a12fb92671c
ht_global <ht15151824103@outlook.com>
2019-10-11 14:54:04 +0800

    add RESTFul

--- src/main/java/com/ht/controller/HelloController.java
+++ src/main/java/com/ht/controller/HelloController.java
@@ -1,8 +1,7 @@
 package com.ht.controller;
 
 import com.ht.entity.Users;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -12,7 +11,30 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public Users hello() {
-        return new Users(1,"贺涛",22,new Date());
+        return new Users(1,"贺涛",22, new Date());
+    }
+
+    /*
+      method = RequestMethod.DELETE,采用了RESTFul风格开发.路径下的/{id}是删除所需的id在地址栏加上id,
+      方法内即可通过@PathVariable注解获取到
+      @DeleteMapping,可以替代@RequestMapping + method = RequestMethod.DELETE
+      @PathVariable注解 表示获取路径上的id,就是地址可以写成 /users/1,形参id就是 1 了
+    */
+    //@RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/users/{id}")
+    public int del(@PathVariable int id) {
+        System.out.println(id);
+        return id;
+    }
+
+    /*
+      @RequestParam注解:可接收页面的值,如表单提交的值.只要变量的值和表单(或地址中的变量名)的name值一致,可有省略不写
+      @RequestBody注解:可接收页面复杂的值,如Json串,集合等
+    */
+    @PostMapping("/users")
+    public Users add(@RequestBody Users user) {
+        System.out.println(user.getName());
+        return user;
     }
 
 }
--- src/main/java/com/ht/entity/Users.java
+++ src/main/java/com/ht/entity/Users.java
@@ -6,20 +6,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
 
+//@JsonPropertyOrder(value = {"age", "name"})此注解改变类中定义的属性的顺序
+//@JsonPropertyOrder(value = {"age", "name"})
 public class Users {
 
     //此注解会在转Json时，屏蔽此属性，就是忽略掉这个字段
     @JsonIgnore
     private int id;
 
-    //此注解可在转Json时自定义字符串，替换原字段值
+    //此注解可在转Json时自定义字符串，替换原属性值,就是为属性起一个别名
     @JsonProperty("xxxx")
     private String name;
 
+    //@JsonInclude(JsonInclude.Include.NON_NULL) 此注解排除属性为空的,就是不会序列化属性值为空的.
     private int age;
 
-    //在转Json时，添加timezone = "GIM+8",因为时区存在差异，会有8个小时的误差，所有加上8即可
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GIM+8")
+    //在转Json时，添加timezone = "GMT+8",因为时区存在差异，会有8个小时的误差，所有加上8即可
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date birthday;
 
     public int getId() {
